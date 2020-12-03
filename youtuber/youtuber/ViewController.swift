61fb61443afc6517fc27d20dc5ee5a831228e926
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-12-03 15:35:05 +0300

    created project

---
+++ youtuber/youtuber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtuber
+//
+//  Created by Anton Borisenko on 12/3/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
