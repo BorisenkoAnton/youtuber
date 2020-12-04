51c72f9a3e9b9685135af80650ebea5c4403d45c
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-12-04 10:35:14 +0300

    added cell

---
+++ youtuber/youtuber/Main Screen/View/VideosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideosTableViewCell.swift
+//  youtuber
+//
+//  Created by Anton Borisenko on 12/4/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import UIKit
+
+class VideosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
