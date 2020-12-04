e4d7fa9e196b6c73f4a5699bd54b20aaebcb1c15
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-12-04 15:29:06 +0300

    added player view controller delegate

---
+++ youtuber/youtuber/Player Screen/View/PlayerViewControllerDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerViewControllerDelegate.swift
+//  youtuber
+//
+//  Created by Anton Borisenko on 12/4/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import Foundation
+
+protocol PlayerViewControllerDelegate: class {
+    
+    func setPlayerPresenterDelegate(delegate: PlayerPresenterDelegate)
+    func setDescription(description: String)
+}
