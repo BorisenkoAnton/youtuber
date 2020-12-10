f47c22ea9d214ba9069a7c35e45abd6ec43e2362
Anton Borisenko <ant.borisenko2013@gmail.com>
2020-12-10 16:38:14 +0300

    added struct to contain all network configuration values

---
+++ youtuber/youtuber/Networking/NetworkConfiguration.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkConfiguration.swift
+//  youtuber
+//
+//  Created by Anton Borisenko on 12/10/20.
+//  Copyright © 2020 Anton Borisenko. All rights reserved.
+//
+
+import Foundation
